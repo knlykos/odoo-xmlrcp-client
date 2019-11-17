a35d917e73a0ba968e67e091a85a360bd32bc814
knlykos <nlopezg87@gmail.com>
2019-11-17 07:40:18 -0800

    actualizacion archivo seguro

--- picking/src/shared/config/odoo-config.ts
+++ picking/src/shared/config/odoo-config.ts
@@ -1,7 +1,7 @@
 export const odooConfig = {
   uid: 2,
   db: 'demo',
-  odooUrl: 'http://localhost:8069',
+  odooUrl: 'https://odoo.nkodexsoft.com',
   username: 'administrador',
   password: '123456',
 };
